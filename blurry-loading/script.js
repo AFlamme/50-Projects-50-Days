51dd9248d7857d65c00f695c7e873b8e6eb87140
Andrey Gneushev <andrey_gneushev@mail.ru>
2022-08-08 17:39:35 +0300

    Проект пятый

--- blurry-loading/script.js
+++ blurry-loading/script.js
@@ -2,7 +2,6 @@ const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
 
 let load = 0
-
 let int = setInterval(blurring, 30)
 
 function blurring() {
@@ -17,7 +16,6 @@ function blurring() {
   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
-// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 }
